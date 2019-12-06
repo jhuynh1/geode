3a1a53ec42f4c8dd7cafcdd25c32de65f7f985cc
Jason Huynh <huynhja@gmail.com>
2019-12-06 13:50:46 -0800

    moved the sleep

--- geode-core/src/main/java/org/apache/geode/internal/cache/CacheDistributionAdvisor.java
+++ geode-core/src/main/java/org/apache/geode/internal/cache/CacheDistributionAdvisor.java
@@ -175,13 +175,13 @@ public class CacheDistributionAdvisor extends DistributionAdvisor {
             }
             return cp.cachedOrAllEventsWithListener();
           }));
+          adviseAllEventsVersion = tempProfilesVersion;
+          adviseInRecoveryVersion = tempInRecoveryVersion;
           try {
             Thread.sleep(500);
           } catch (InterruptedException e) {
             e.printStackTrace();
           }
-          adviseAllEventsVersion = tempProfilesVersion;
-          adviseInRecoveryVersion = tempInRecoveryVersion;
         }
       }
     }
